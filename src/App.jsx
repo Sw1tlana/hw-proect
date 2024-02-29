b27dde702958ddd54b1f85ac484f02a8d006b61f
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-02-29 22:16:53 +0200

    Add state in components MailBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
+import { useState } from 'react';
 import ProductGallery from './components/ProductCard/ProductGallery';
 import productCard  from './productCard.json';
 import Mailbox from './components/MailBox/Mailbox';
 
+const emailsData = [
+  { id: "1", email: "rex135@gmail.com"},
+  { id: "2", email: "asdg@SDFH.COM"},
+  { id: "3", email: "xcvbh@zsgsdfhj.com"},
+]
+
 function App() {
+const [counter, setCounter] = useState(0);
+const [emails, setEmails] = useState(emailsData);
+const [showMailBox, setshowMailBox] = useState(false);
+
  const onLogEmail = () => {
   console.log('Email.for sent')
+  setCounter(counter + 1);
  }
 
  const handleDelete = (mailId) => {
-  console.log("mailId", mailId)
+  console.log("mailId", mailId);
+  setEmails(prevState => prevState.filter((email) => email.id !== mailId));
+}
+
+const handleMailBox = () => {
+  setshowMailBox(prevState => !prevState);
 }
 
   return (
     <div>
+      <h2>Email counts: {counter}</h2>
+      <button type="button" onClick={handleMailBox}>{showMailBox ? "Hide" : "Show"} mail Box</button>
+      {showMailBox ? (<Mailbox emails={emails} 
+      emailCounter={counter}
+      onLogEmail={onLogEmail} 
+      onDeleteEmail={handleDelete}/>) : null}
       <ProductGallery productCard={productCard}/>
-      <Mailbox onLogEmail={onLogEmail} onDeleteEmail={handleDelete}/>
  </div>
       
   )
--- src/components/MailBox/Mailbox.jsx
+++ src/components/MailBox/Mailbox.jsx
@@ -1,14 +1,20 @@
 
-const Mailbox = ({ onLogEmail, onDeleteEmail }) => {
+const Mailbox = ({ emails, onLogEmail, onDeleteEmail, emailCounter }) => {
 
   return (
     <div>
-        <h2>MailBox</h2>
+        <h2>MailBox: {emailCounter}</h2>
        <ul>
-        <li>Mail-1<button onClick={() => onDeleteEmail(1)} type="button">😵</button></li>
+      {emails.map((email) => 
+      <li key={email.id}>
+       {email.email}{' '}
+       <button onClick={() => onDeleteEmail(email.id)} type="button">😵</button>
+      </li>
+      )}
+        </ul>   
+        {/* <li>Mail-1<button onClick={() => onDeleteEmail(1)} type="button">😵</button></li>
         <li>Mail-2<button onClick={() => onDeleteEmail(2)} type="button">😵</button></li>
-        <li>Mail-3<button onClick={() => onDeleteEmail(3)} type="button">😵</button></li>
-       </ul>
+        <li>Mail-3<button onClick={() => onDeleteEmail(3)} type="button">😵</button></li> */}
        <button onClick={onLogEmail} type='button' >Send mail</button>
     </div>
   )
